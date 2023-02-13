8714c9e0db020b53893ad17d9a07508dbd84aeba
Mostafa-Mohammedi <121897256+Mostafa-Mohammedi@users.noreply.github.com>
2023-02-13 12:07:34 +0100

    added the global variable for the html element

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// global variable 
+const bankDeposit = document.getElementById("bank-deposit");
+const loanBank = document.getElementById("loan-bank");
+const totalBalance = document.getElementById("totalBalance");
+const loan = document.getElementById("loan");
+const payFullLoan = document.getElementById("pay-full-loan");
+const pay = document.getElementById("pay");
+const bank = document.getElementById("bank");
+const work = document.getElementById("work");
+const dataOutput = document.getElementById("get-data-api");
+const dataSpec = document.getElementById("data-output");
+const laptopHeader = document.getElementById("laptop-header");
+const def = document.getElementById("default");
+const img = document.getElementById("img");
+const price = document.getElementById("price");
+const buy = document.getElementById("buy");
+
+let salary = 100;
+payFullLoan.style.visibility = 'hidden';
+payFullLoan.disabled = true;
+
+
